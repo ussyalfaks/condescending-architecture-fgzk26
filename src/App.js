9431c76fd99c0779c107c4efd71cd2bcda809b24
CodeSandbox <support@codesandbox.io>
2023-08-27 21:13:52 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header>
+        <h3 className="logo">𝑢𝑠𝑚𝑎𝑛</h3>
+        <ul className="nav">
+          <li>
+            <a href=""> Services</a>
+          </li>
+          <li>
+            <a href=""> Works</a>
+          </li>
+          <li>
+            <a href=""> Notes</a>
+          </li>
+          <li>
+            <a href=""> Experince</a>
+          </li>
+        </ul>
+        <h4>+234 8166 280 639</h4>
+      </header>
+
+      <section className="section-one">
+        <div className="sec-one">
+          <div className="greeting-sec">
+            <h1>Hey there i'm Usman</h1>
+          </div>
+          <div className="what-sec">
+            <h5>i design beatifully simple things, i love what i do.</h5>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
